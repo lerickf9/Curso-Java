6ad586cc03e5aac3b662ca30c303fc596d4f2d69
Erick <efuentes.edf9@gmail.com>
2024-04-04 23:38:06 -0300

    Terminando la clase decisiones

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -3,12 +3,20 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPElicula = 8.2;
+        String tipoPlan = "plus";
+
         if(fechaDeLanzamiento >= 2022){
             System.out.println("Peliculas más populares");
         }
         else{
             System.out.println("Peliculas retro qué aún vale la pena ver");
             System.out.println();
+        }
+
+        if(incluidoEnElPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute del plan "+tipoPlan);
+        }else {
+            System.out.println("No posee el plan "+tipoPlan);
         }
     }
 
