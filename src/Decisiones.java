cbe64fb54ece1eb04e0021378071979775bd4700
Erick <efuentes.edf9@gmail.com>
2024-04-04 23:29:22 -0300

    Añadiendo la clase decisiones

---
+++ src/Decisiones.java
@@ -0,0 +1,15 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPElicula = 8.2;
+        if(fechaDeLanzamiento >= 2022){
+            System.out.println("Peliculas más populares");
+        }
+        else{
+            System.out.println("Peliculas retro qué aún vale la pena ver");
+            System.out.println();
+        }
+    }
+
+}
